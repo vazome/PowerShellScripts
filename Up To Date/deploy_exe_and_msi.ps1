4230c90d508a22dd0db701ddb4069d970e0aa272
Daniel Vazome <danielvazome@outlook.com>
2020-05-21 11:53:47 +0300

    Deletion of submodule

---
+++ Up To Date/deploy_exe_and_msi.ps1
@@ -0,0 +1,9 @@
+﻿$locationfiles = Read-Host -Prompt "Specify the .msi or .exe containing folder"
+$installers = Get-ChildItem -Path $locationfiles -Filter "*.exe"
+foreach ($install in $installers){
+    Start-Process -Wait -FilePath ($install.FullName) -PassThru
+}
+$installers = Get-ChildItem -Path $locationfiles -Filter "*.msi"
+foreach ($install in $installers){
+    Start-Process -Wait -FilePath ($install.FullName) -PassThru
+}
