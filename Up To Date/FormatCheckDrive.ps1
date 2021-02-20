dd53feb777013df9a76532efa22f751631179c1f
Daniel Vazome <46573198+vazome@users.noreply.github.com>
2021-02-20 18:24:41 +0300

    Create FormatCheckDrive.ps1

---
+++ Up To Date/FormatCheckDrive.ps1
@@ -0,0 +1,11 @@
+#First, connect your HDD or SSD
+#If CristalDisk Info was started we are stopping it
+Stop-Process -Name DiskInfo64
+#Cleaning whole drive and removing partitions
+#WARNING CHANGE THE DISK NUMBER VALUE TO ACCORDING VALUE IN ORDER TO PREVENT UNINTENTINAL DATA LOOSE
+Clear-Disk -Number 1 -RemoveData
+#Closing all incomming windows signaling that we have connected our drive and cleaned it
+Stop-Process -Name explorer
+Start-Process explorer -ArgumentList "/n,/select,%SystemDrive%"
+Start-Process -FilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\CrystalDiskInfo\CrystalDiskInfo (64bit).lnk"
+get-disk
